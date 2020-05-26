79d8b55b4caa26d706c777f5fbd3347fc50bfde4
Halim <halim.izzouzi@kpf-si.com>
2020-05-26 11:09:36 +0200

    ajout components home, signin, signup

--- projet-vero-frontend/src/app/app.module.ts
+++ projet-vero-frontend/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
